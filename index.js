721db811f71a1e8a4d6c01d8b4ff0a42051b9ca3
Neel2377 <riderh911@gmail.com>
2025-08-08 19:22:16 +0530

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 const db = require('./config/db');
 const bodyParser = require('body-parser');
 db();
